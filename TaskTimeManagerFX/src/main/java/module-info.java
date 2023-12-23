5d8d7892c393b7af392f720dff5008f514c74a9e
Exspiria <146676744+BaldurusExspa@users.noreply.github.com>
2023-12-23 03:29:11 +0300

    UpdateLine

--- TaskTimeManagerFX/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.tasktimemanagerfx {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.tasktimemanagerfx to javafx.fxml;
-    exports com.example.tasktimemanagerfx;
-}
